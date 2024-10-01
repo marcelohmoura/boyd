205631b96cda620baf9630ddfdcddf17eb2a3a4d
Marcelo Moura <marcelohmoura99@outlook.com.br>
2024-09-30 23:01:32 -0300

    test: Product registration (Without persistence)

--- test/boyd/core_test.clj
+++ test/boyd/core_test.clj
@@ -10,29 +10,22 @@
 
 (deftest handler
 
-  (testing "Test GET request to /hello?name={a-name} returns expected response"
-    (let [response (app (-> (mock/request :get  "/api/plus?x=1&y=2")))
-          body     (parse-body (:body response))]
-      (is (= (:status response) 200))
-      (is (= (:result body) 3))))
+  #_(testing "Test GET request to /hello?name={a-name} returns expected response"
+      (let [response (app (-> (mock/request :get "/api/plus?x=1&y=2")))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:result body) 3))))
 
-  (testing "Should to register a valid product"
-    (with-redefs [helpers/->uuid (fn [] #uuid"2f7f1ec2-5ada-4e63-a595-9ab83c839e8b")]
-      (let [product {:name        "iPhone 16 Plus"
-                     :price       1999.0
-                     :category    :smartphone
-                     :description "A new 2024 iPhone model!"}
-
-            product-register-result {:id          #uuid"2f7f1ec2-5ada-4e63-a595-9ab83c839e8b"
-                                     :name        "iPhone 16 Plus"
-                                     :price       1999.0
-                                     :category    :smartphone
-                                     :description "A new 2024 iPhone model!"}
+  (with-redefs [helpers/->uuid (fn [] #uuid"2f7f1ec2-5ada-4e63-a595-9ab83c839e8b")]
+    (testing "Test POST request to /echo returns expected response"
+      (let [product {:name        "Laptop Lenovo Ideapad"
+                     :price       1958.58
+                     :category    "Laptops"
+                     :description "A Good Laptop"}
             response (app (-> (mock/request :post "/api/register")
                               (mock/content-type "application/json")
                               (mock/body (cheshire/generate-string product))))
             body (parse-body (:body response))]
         (is (= (:status response) 200))
-        (is (= body product-register-result))))))
-
-
+        (is (= body
+               (assoc product :id "2f7f1ec2-5ada-4e63-a595-9ab83c839e8b")))))))
